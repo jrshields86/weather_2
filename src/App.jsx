d7715f11e6118f0e068a9359d261765eb988e930
jrshields86 <jrshields86@gmail.com>
2024-04-22 08:56:14 -0600

    get request working

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import axios from 'axios';
 import './App.css'
 
 function App() {
-
-// const lat = 39.65;
-// const lon = -105.29;
-// const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-// const BASE_URL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_KEY}`;
-// console.log(BASE_URL);  
-
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   const fetchWeather = async()=>{
     const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=39.65&lon=-105.29&exclude={part}&appid=${API_KEY}`);
