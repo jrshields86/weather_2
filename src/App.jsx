026a98708bd4c2bd4fd2fcc9f4cf864adca53341
jrshields86 <jrshields86@gmail.com>
2024-04-28 16:30:01 -0600

    current weather starting to take shape

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,44 @@ function App() {
   const [current, setCurrent] = useState({});
   const [forecast, setForecast] = useState([]);
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+  
 
+  useEffect(()=> {
+    const fetchWeather = async()=>{
+      const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=39.65&lon=-105.29&exclude={part}&appid=${API_KEY}&units=imperial`);
+      const weatherData = response.data.current;
+      const forecastData = response.data.daily;
+      setCurrent(weatherData);
+      setForecast(forecastData);
+    };
+    fetchWeather();
+  }, []);
 
-useEffect(()=> {
-  const fetchWeather = async()=>{
-    const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=39.65&lon=-105.29&exclude={part}&appid=${API_KEY}&units=imperial`);
-    const weatherData = response.data.current;
-    const forecastData = response.data.daily;
-    setCurrent(weatherData);
-    setForecast(forecastData);
+  function windDirection(deg) {
+    if (deg > 337.5 || deg <= 22.25) {
+        return 'N'
+    } else if (deg > 22.25 && deg <= 67.5) {
+        return 'NE'
+    } else if (deg > 67.5 && deg <= 112.5) {
+        return 'E'
+    } else if (deg > 112.5 && deg <= 157.5) {
+        return 'SE'
+    } else if (deg > 157.5 && deg <= 202.5) {
+        return 'S'
+    } else if (deg > 202.5 && deg <= 247.5) {
+        return 'SW'
+    } else if (deg > 247.5 && deg <= 292.5) {
+        return 'W'
+    } else if (deg > 292 && deg <= 337.5) {
+        return 'NW'
+    }
   };
-  fetchWeather();
-}, []);
 
-console.log(current);
 
+  console.log(current);
   return (
     <>
-      {current && <CurrentWeather current={current} />}
+      {current && <CurrentWeather current={current} windDirection={windDirection} />}
     </>
   )
 }
--- src/CurrentWeather.jsx
+++ src/CurrentWeather.jsx
@@ -1,27 +1,47 @@
 import './App.css';
 
-const CurrentWeather = ({current}) => {
-    console.log(current);
+const CurrentWeather = ({current, windDirection}) => {
+    
     return (
-        <>
+        <div className='app'>
             <h1>Current Weather</h1>
             <div className='weatherContainer'>
-                <p>Temp: {Math.round(current.temp)}°F</p>
-                <p>Feels Like: {Math.round(current.feels_like)}°F</p>
-                <p>Pressure: {current.pressure} hPa</p>
-                <p>Humidity: {current.humidity}%</p>
-                <p>Dew Point: {Math.round(current.dew_point)}°F</p>
-                <p>UV Index: {Math.round(current.uvi)} of 11</p>
-                <p>Wind Speed: {current.wind_speed} mph</p>
-                <p>Wind Direction: {current.wind_deg} °</p>
-                <p>Wind Gust: {current.wind_gust} mph</p>
+                <div className='top'>
+                    <p className='temp'>{Math.round(current.temp)}°F</p>
+                    <p className='feelsLike'>Feels Like: {Math.round(current.feels_like)}°F</p>
+                </div>
+                <div className='bottom'>
+                    <p>Pressure
+                        <br/>
+                        {current.pressure} hPa
+                    </p>
+                    <p>Humidity 
+                        <br/>
+                        {current.humidity} %
+                    </p>
+                    <p>Dew Point
+                        <br/>
+                        {Math.round(current.dew_point)} °F
+                    </p>
+                    <p>UV Index
+                        <br/>
+                        {Math.round(current.uvi)} of 11
+                    </p>
+                    <p>Wind
+                        <br/>
+                        {windDirection(current.wind_deg)} {current.wind_speed} mph
+                    </p>
+                    <p>Wind Gust
+                        <br/>
+                        {current.wind_gust} mph
+                    </p>
+                </div>
                 {/* <p>Weather Description: {current.weather[0].description}</p>
                 <p>Weather Icon: {current.weather[0].icon}</p>
                 <p>Weather Main: {current.weather[0].main}</p>
                 <p>Weather ID: {current.weather[0].id}</p> */}
-                {/* <p>Snow in Last Hour: {Object.values(current.snow)} mm/hr</p> */}
             </div>
-        </>
+        </div>
     );
 };
 
