276823fb71fe4474fa9686d544ffbfd4718da7c1
jrshields86 <jrshields86@gmail.com>
2024-05-04 21:50:04 -0600

    progress on Forecast component

--- src/CurrentWeather.jsx
+++ src/CurrentWeather.jsx
@@ -2,6 +2,7 @@ import './App.css';
 
 const CurrentWeather = ({current, windDirection}) => {
     const iconData = current.weather;
+    console.log(iconData);
 
     if (!iconData){
         return null;
--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,14 +1,41 @@
 import './App.css'
 
 const Forecast = ({forecast, windDirection}) => {
-    const tomorrowData = forecast.daily;
-    console.log(tomorrowData);
+    if(!forecast.daily){
+        return null;
+    }
+
+    const forecastData = forecast.daily;
+    console.log(forecastData);
+    const formattedForecast = forecastData.slice(1, forecastData.length);
+    console.log(formattedForecast);
+
+    // const tomorrowData = forecast.daily[1].weather[0].icon;
+    // console.log(tomorrowData);
     
 
     return (
         <div>
             <div className="forecastContainer">
-                
+                {/* <div className='forecastLeft'>
+                    <p>Date</p>
+                    <img alt='weather icon' className='weatherIcon' src={`icons/${tomorrowData}.png`}/>
+                </div>
+                <div>
+                    <p>Min:</p>
+                    <p>Max:</p>
+                </div> */}
+                    {
+                        formattedForecast.map(el =>{
+                            return(
+                                <div className='forecastRight'>
+                                 Min: {el.temp.min}
+                                 <br/>
+                                 Max: {el.temp.max}
+                                </div>
+                            )
+                        })
+                    }
             </div>
         </div>
     );
