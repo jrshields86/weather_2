6687961a1dc7e1014c28e7bcfb4d2e246d672457
jrshields86 <jrshields86@gmail.com>
2024-06-11 19:07:24 -0600

    alerts component functioning

--- src/Alerts.jsx
+++ src/Alerts.jsx
@@ -1,24 +1,55 @@
 import './App.css'
+import React, { useState } from "react";
+import './App.css'
+
 
 
 const Alerts = ({forecast}) => {
     if (!forecast.alerts) {
         return null;
     };
+    const [modal, setModal] = useState(false);
     const alertTitle = forecast.alerts[0].event;
     const alertStartTime = new Date(forecast.alerts[0].start * 1000);
     const alertEndTime = new Date(forecast.alerts[0].end * 1000);
     const alertDescription = forecast.alerts[0].description;
-    console.log(alertDescription);
     
+    const toggleModal = () => {
+        setModal(!modal)
+    };
+
+    if (modal) {
+        document.body.classList.add('active-modal')
+    } else {
+        document.body.classList.remove('active-modal')
+    }
 
 
     return (
+        
         <div>
-            <div className='alertContainer'>
-                {alertTitle}
+            <button className="btn-modal" onClick={toggleModal}>
+                    {alertTitle}
+            </button>
+            {modal && (
+            <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content">
+                <h2>{alertTitle}</h2>
+                <p>
+                {alertDescription}
+                </p>
+                <button className="close-modal" onClick={toggleModal}>
+                CLOSE
+                </button>
             </div>
+            </div>
+        )}
+
         </div>
+
+
+        
     );
 };
 
