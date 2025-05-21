9a9695aed742bd72906855f9a1baf4c9707a8fa5
jrshields86 <jrshields86@gmail.com>
2025-05-20 18:58:11 -0600

    current weather layout changes

--- src/CurrentWeather.jsx
+++ src/CurrentWeather.jsx
@@ -18,52 +18,49 @@ const CurrentWeather = ({current, windDirection}) => {
                     Current Weather
                 </div>
                 <div className='leftAndRightContainer'>
-                    <div className='leftContainer'>
-                        <div className='topContainer'>
-                            <div className='top-left'>
-                                <p className='temp'>{Math.round(current.temp)}°F</p>
-                                <p className='feelsLike'>Feels Like: {Math.round(current.feels_like)}°F</p>
-                            </div>    
-                            <div className='top-right'>
-                                <img alt='weather icon' className='currentWeatherIcon' src={`icons/${icon}.png`}/>
-                                <div className='descriptionContainer'>
-                                    <p className='description'>{description}</p>
-                                </div>
+                        <div className='top-left'>
+                            <p className='temp'>{Math.round(current.temp)}°F</p>
+                            <p className='feelsLike'>Feels Like: {Math.round(current.feels_like)}°F</p>
+                        </div>    
+                        <div className='top-right'>
+                            <img alt='weather icon' className='currentWeatherIcon' src={`icons/${icon}.png`}/>
+                            <div className='descriptionContainer'>
+                                <p className='description'>{description}</p>
                             </div>
                         </div>
-                        <div className='bottomContainer'>
-                            <div className='bottomUpper'>
-                                <div>
-                                    <p className='detailTitle'>Wind</p>
-                                    <p className='detailValue'>{windDirection(current.wind_deg)} {Math.round(current.wind_speed)} mph</p>
-                                </div>
-                                <div>
-                                    <p className='detailTitle'>Clouds</p>
-                                    <p className='detailValue'>{current.clouds}%</p>
-                                </div>
-                                {/* <div>
-                                    <p className='detailTitle'>Humidity </p>
-                                    <p className='detailValue'>{current.humidity} %</p>
-                                </div> */}
-                            </div>
-                            {/* <div className='bottomMiddle'>
-                                <div>
-                                    <p className='detailTitle'>Dew Point</p>
-                                    <p className='detailValue'>{Math.round(current.dew_point)} °F</p>
-                                </div>
-                            </div> */}
+                        
+                        <div id='currentWeatherWind'>
+                            <p className='detailTitle'>Wind</p>
+                            <p className='detailValue'>{windDirection(current.wind_deg)} {Math.round(current.wind_speed)} mph</p>
                         </div>
-                        {/* <div className='bottomLowerContainer'>
-                                <div>
-                                    <p className='detailTitle'>UV Index</p>
-                                    <p className='detailValue'>{Math.round(current.uvi)} of 11</p>
-                                </div>
-                                <div>
-                                    <p className='detailTitle'>Pressure</p>
-                                    <p className='detailValue'>{(current.pressure * 0.025).toFixed(2)} in</p>
-                                </div>
+                        <div id='currentWeatherClouds'>
+                            <p className='detailTitle'>Clouds</p>
+                            <p className='detailValue'>{current.clouds}%</p>
+                        </div>
+
+                        {/* <div>
+                            <p className='detailTitle'>Humidity </p>
+                            <p className='detailValue'>{current.humidity} %</p>
                         </div> */}
-                    </div>
+                        
+                        {/* <div className='bottomMiddle'>
+                            <div>
+                                <p className='detailTitle'>Dew Point</p>
+                                <p className='detailValue'>{Math.round(current.dew_point)} °F</p>
+                            </div>
+                        </div> */}
+                    
+                    {/* <div className='bottomLowerContainer'>
+                            <div>
+                                <p className='detailTitle'>UV Index</p>
+                                <p className='detailValue'>{Math.round(current.uvi)} of 11</p>
+                            </div>
+                            <div>
+                                <p className='detailTitle'>Pressure</p>
+                                <p className='detailValue'>{(current.pressure * 0.025).toFixed(2)} in</p>
+                            </div>
+                    </div> */}
+                    
                 </div>
             </div>
         </div>
